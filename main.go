3824a7200ccdb2310ffd89c6e1a43d9a4fecb81f
Vu Hoang <baovuhbv@gmail.com>
2021-03-14 11:27:36 +0700

    var

--- main.go
+++ main.go
@@ -5,14 +5,12 @@ import "fmt"
 func main() {
 	fmt.Println("Hello Golang")
 
-	fmt.Println("Sum", Sum(4))
-}
+	var number int
+	number = 10
 
-func Sum(number int) int {
-	sum := 0
-	for i := 0; i < number; i++ {
-		sum += i
-	}
+	fmt.Println(number)
 
-	return sum
+	// Type Inference
+	var email = "baovuhbv@gmail.com"
+	fmt.Println(email)
 }
